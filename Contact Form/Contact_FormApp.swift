e28ea7fd0d5a6813a284d193908e2ede4639ac7a
alexguynichols <alexguynichols@daxta.co.uk>
2022-08-25 15:41:53 +0100

    Initial Commit

---
+++ Contact Form/Contact_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contact_FormApp.swift
+//  Contact Form
+//
+//  Created by Alex Nichols on 25/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Contact_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
